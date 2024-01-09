7d8055983534a5c9610528c249731a5ca9071f3e
Asma Eman <56062252+AsmaEman@users.noreply.github.com>
2024-01-09 08:01:25 +0500

    Update evidence_summary_api.py

--- streamlit_app/evidence_summary_api.py
+++ streamlit_app/evidence_summary_api.py
@@ -3,8 +3,8 @@ from flask import Flask, request, jsonify
 import threading
 import requests 
 import pickle
-from pages.File_Report import Chatbot
-
+from pages.File_Report import Chatbot, process_file
+import base64
 
 file = open('LLM_Index', 'rb')
 
@@ -14,24 +14,27 @@ LLM_Index = pickle.load(file)
 file.close()
 bot = Chatbot("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", index=LLM_Index)
 app = Flask(__name__)
-
 @app.route('/summarize_evidence', methods=['POST'])
 def summarize_evidence():
-    if request.content_type.startswith('multipart/form-data'):
-        file = request.files.get('file')
+    # Check if there is a file in the request
+    if 'file' in request.files:
+        file = request.files['file']
+
+        # Ensure the file is not empty
         if file and file.filename:
+            
             if file.content_type.startswith('image/'):
-                summary = bot.summarize_image_evidence(file.read())
+                # Process image file
+                summary = bot.evidence_image(process_file(file))
             else:
-                summary = bot.summarize_text_evidence(file.read())
+                # Process other file types (e.g., PDF, DOCX)
+                summary = bot.generate_evidence_summary(process_file(file))
         else:
             return jsonify({'error': 'No file provided'}), 400
-    elif request.content_type.startswith('application/json'):
-        text_data = request.json.get('text_data', '')
-        summary = bot.summarize_text_evidence(text_data)
     else:
-        return jsonify({'error': 'Unsupported Media Type'}), 415
+        return jsonify({'error': 'No file part in the request'}), 400
 
     return jsonify({'summary': summary})
+
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
